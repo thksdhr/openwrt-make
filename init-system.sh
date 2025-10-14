d2512fb3af82a600d776d5e87b8a3c3a80801bc7
thksdhr <thksdhr@icloud.com>
2025-10-14 17:52:42 +0800

    test

---
+++ init-system.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# install development tools for Ubuntu/Debian
+sudo apt update
+sudo apt install -y build-essential git-core libncurses5-dev libncursesw5-dev zlib1g-dev \
+    gawk flex gettext wget unzip python3 rsync subversion gcc-multilib g++-multilib libssl-dev \
+    libelf-dev ecj fastjar java-propose-classpath genisoimage
