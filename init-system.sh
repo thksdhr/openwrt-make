d3a9a247b53f62eef5c24323dd8a8a084175c6b6
thksdhr <thksdhr@icloud.com>
2025-10-19 20:37:40 +0800

    更新 GitHub Actions 脚本

--- init-system.sh
+++ init-system.sh
@@ -3,4 +3,4 @@
 sudo apt update
 sudo apt install -y build-essential git-core libncurses5-dev libncursesw5-dev zlib1g-dev \
     gawk flex gettext wget unzip python3 rsync subversion gcc-multilib g++-multilib libssl-dev \
-    libelf-dev ecj fastjar java-propose-classpath genisoimage
+    libelf-dev ecj fastjar java-propose-classpath genisoimage python3 python3-dev
