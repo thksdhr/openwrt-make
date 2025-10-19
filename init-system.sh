cf4ab328b6017694a11e8da9fb5ec9a2225e67ff
thksdhr <thksdhr@icloud.com>
2025-10-19 21:35:38 +0800

    更新 GitHub Actions 脚本

--- init-system.sh
+++ init-system.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y \
   autoconf automake libtool cmake ninja-build bc m4 patch fakeroot quilt \
   file sudo time jq locales \
   git git-lfs subversion mercurial wget curl rsync unzip \
-  python3 python3-dev python3-pip python3-wheel python3-distutils \
+  python3 python3-dev python3-pip python3-wheel \
   libncurses5-dev libncursesw5-dev libncurses-dev zlib1g-dev libssl-dev libelf-dev \
   libglib2.0-dev libfuse-dev libcap-dev libattr1-dev libdw-dev uuid-dev \
   liblzma-dev libzstd-dev libbz2-dev libxml2-utils \
