4638bd2c5e53cac5f8f9890a8dd51a8e8963aee4
taozig <thksdhr@gmail.com>
2026-07-27 01:02:30 +0800

    新增了 uci 脚本

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 SHELL_PATH=$(readlink -f "${BASH_SOURCE[0]}")
 SHELL_DIR=$(dirname ${SHELL_PATH})
 echo "SHELL_PATH: ${SHELL_PATH}"
 echo "SHELL_DIR: ${SHELL_DIR}"
+SHELL_TIME=$(date +%Y.%m.%d-%H.%M.%S)
 
-# 环境变量
+# 自定义环境变量
 BuildVersion="v25.12.5"
 
 # 更新包管理器缓存
@@ -43,11 +45,14 @@ git checkout ${BuildVersion}
 ######################################################################
 
 cp ${SHELL_DIR}/config ${SHELL_DIR}/openwrt/.config
+cp ${SHELL_DIR}/files ${SHELL_DIR}/openwrt/
 
 ######################################################################
 # 开始编译
 ######################################################################
 
-(time make world V=s -j$(nproc)) 2>&1 | tee -a ${SHELL_DIR}/make.log
+mkdir -p ${SHELL_DIR}/logs/
+
+(time make world V=s -j$(nproc)) 2>&1 | tee -a ${SHELL_DIR}/logs/${SHELL_TIME}.log
 
 echo "END"
